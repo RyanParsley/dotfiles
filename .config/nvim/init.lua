22f4ac97b4314fd7e71244880d8de259f2ede983
Ryan Parsley <Ryan.Parsley@JBHunt.com>
2023-05-04 16:28:49 -0400

    feat: configure neotree visibility

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,10 +89,28 @@ require('lazy').setup({
     }, {
       "nvim-neo-tree/neo-tree.nvim",
       branch = "v2.x",
-      dependencies = { 
+      dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
+      },
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = false,
+            show_hidden_count = false,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+             '.git',
+             '.DS_Store',
+             'thumbs.db',
+            },
+            never_show = {
+             '.DS_Store',
+            },
+          }
+        }
       }
     }, { -- Autocompletion
         'hrsh7th/nvim-cmp',
