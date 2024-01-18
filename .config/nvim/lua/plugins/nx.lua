eae47b4ec8a7119a26dfe1adbe821c406873b0b2
Ryan Parsley <RMParsley@gmail.com>
2024-01-18 11:21:55 -0500

    chore: clean up nx config a little

--- .config/nvim/lua/plugins/nx.lua
+++ .config/nvim/lua/plugins/nx.lua
@@ -1,24 +1,17 @@
 return {
-    'Equilibris/nx.nvim',
-    dependencies = {'nvim-telescope/telescope.nvim'},
-    config = function()
-        require('nx').setup {
-            -- Base command to run all other nx commands, some other values may be:
-            -- - `npm nx`
-            -- - `yarn nx`
-            -- - `pnpm nx`
-            nx_cmd_root = 'npx nx',
+    {
+        "Equilibris/nx.nvim",
 
-            -- Command running capabilities,
-            -- see nx.m.command-runners for more details
-            command_runner = require('nx.command-runners').terminal_cmd(),
-            -- Form rendering capabilities,
-            -- see nx.m.form-renderers for more detials
-            form_renderer = require('nx.form-renderers').telescope(),
+        dependencies = {"nvim-telescope/telescope.nvim"},
 
-            -- Whether or not to load nx configuration,
-            -- see nx.loading-and-reloading for more details
-            read_init = true
+        opts = {
+            -- See below for config options
+            nx_cmd_root = "npx nx"
+        },
+
+        -- Plugin will load when you use these keys
+        keys = {
+            {"<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions"}
         }
-    end
+    }
 }
