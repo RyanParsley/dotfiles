3b3aae9d2ef6412e474726211bf71dfa823f56f5
Ryan Parsley <RMParsley@gmail.com>
2024-03-22 21:58:57 -0400

    chore: remove plugins that are problematic on my phone

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -2,24 +2,14 @@ return {
     'nvim-neotest/neotest',
     dependencies = {
         'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter',
-        'antoinemadec/FixCursorHold.nvim', 'haydenmeade/neotest-jest',
-        'rouge8/neotest-rust', 'nvim-neotest/nvim-nio'
+        'antoinemadec/FixCursorHold.nvim', 'nvim-neotest/nvim-nio'
     },
     config = function()
         require('neotest').setup {
             noremap = true,
             silent = true,
             nowait = true,
-            adapters = {
-                require 'neotest-jest' {
-                    jestCommand = 'npm test',
-                    env = {CI = true}
-                },
-                require 'neotest-rust' {
-                    args = {'--no-capture'},
-                    dap_adapter = 'lldb'
-                }
-            }
+            adapters = {}
         }
     end
 }
