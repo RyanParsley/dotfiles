464530441852d7bd9bfc049552bfa5491153836a
Ryan Parsley <RMParsley@gmail.com>
2024-06-19 17:00:46 -0400

    feat: add headline back

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -2,66 +2,67 @@ return {
     {
         -- Autoformat
         'stevearc/conform.nvim',
-        event = {"BufReadPost", "BufWritePost", "BufNewFile"},
-        dependencies = {"mason.nvim"},
+        event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+        dependencies = { 'mason.nvim' },
         lazy = true,
-        cmd = "ConformInfo",
+        cmd = 'ConformInfo',
         keys = {
             {
                 -- Customize or remove this keymap to your liking
-                "<leader>f",
+                '<leader>f',
                 function()
-                    require("conform").format({
+                    require('conform').format {
                         async = true,
-                        lsp_format = "fallback"
-                    })
+                        lsp_format = 'fallback',
+                    }
                 end,
-                mode = "",
-                desc = "[F]ormat buffer"
-            }
+                mode = '',
+                desc = '[F]ormat buffer',
+            },
         },
         opts = {
             format = {
                 timeout_ms = 3000,
                 async = false, -- not recommended to change
                 quiet = false, -- not recommended to change
-                lsp_fallback = true -- not recommended to change
+                lsp_fallback = true, -- not recommended to change
             },
             notify_on_error = false,
             format_on_save = function(bufnr)
                 -- Disable "format_on_save lsp_fallback" for languages that don't
                 -- have a well standardized coding style. You can add additional
                 -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = {c = true, cpp = true}
+                local disable_filetypes = { c = true, cpp = true }
                 return {
                     timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
                 }
             end,
             formatters_by_ft = {
-                lua = {'stylua'},
-                fish = {"fish_indent"},
-                sh = {"shfmt"},
+                lua = { 'stylua' },
+                fish = { 'fish_indent' },
+                sh = { 'shfmt' },
                 -- Conform can also run multiple formatters sequentially
                 -- python = { "isort", "black" },
                 --
                 -- You can use a sub-list to tell conform to run *until* a formatter
                 -- is found.
-                javascript = {{'prettierd', 'prettier'}},
-                typescript = {{'prettierd', 'prettier'}},
-                markdown = {'markdownlint', 'prettier'}
-            }
-        }
+                javascript = { { 'prettierd', 'prettier' } },
+                typescript = { { 'prettierd', 'prettier' } },
+                markdown = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
+                ['markdown.mdx'] = { { 'prettierd', 'prettier' }, 'markdownlint', 'markdown-toc' },
+            },
+        },
     },
     {
-        "mfussenegger/nvim-lint",
-        event = {"BufReadPost", "BufWritePost", "BufNewFile"},
+        'mfussenegger/nvim-lint',
+        event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
         opts = {
             -- Event to trigger linters
-            events = {"BufWritePost", "BufReadPost", "InsertLeave"},
+            events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
             linters_by_ft = {
-                fish = {"fish"},
-                markdown = {'markdownlint'}
+                fish = { 'fish' },
+                markdown = { 'markdownlint' },
                 -- Use the "*" filetype to run linters on all filetypes.
                 -- ['*'] = { 'global linter' },
                 -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
@@ -80,20 +81,17 @@ return {
                 --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
                 --   end,
                 -- },
-            }
+            },
         },
         config = function(_, opts)
             local M = {}
 
-            local lint = require("lint")
+            local lint = require 'lint'
             for name, linter in pairs(opts.linters) do
-                if type(linter) == "table" and type(lint.linters[name]) ==
-                    "table" then
-                    lint.linters[name] =
-                        vim.tbl_deep_extend("force", lint.linters[name], linter)
-                    if type(linter.prepend_args) == "table" then
-                        vim.list_extend(lint.linters[name].args,
-                                        linter.prepend_args)
+                if type(linter) == 'table' and type(lint.linters[name]) == 'table' then
+                    lint.linters[name] = vim.tbl_deep_extend('force', lint.linters[name], linter)
+                    if type(linter.prepend_args) == 'table' then
+                        vim.list_extend(lint.linters[name].args, linter.prepend_args)
                     end
                 else
                     lint.linters[name] = linter
@@ -104,7 +102,7 @@ return {
             function M.debounce(ms, fn)
                 local timer = vim.uv.new_timer()
                 return function(...)
-                    local argv = {...}
+                    local argv = { ... }
                     timer:start(ms, 0, function()
                         timer:stop()
                         vim.schedule_wrap(fn)(unpack(argv))
@@ -124,34 +122,33 @@ return {
 
                 -- Add fallback linters.
                 if #names == 0 then
-                    vim.list_extend(names, lint.linters_by_ft["_"] or {})
+                    vim.list_extend(names, lint.linters_by_ft['_'] or {})
                 end
 
                 -- Add global linters.
-                vim.list_extend(names, lint.linters_by_ft["*"] or {})
+                vim.list_extend(names, lint.linters_by_ft['*'] or {})
 
                 -- Filter out linters that don't exist or don't match the condition.
-                local ctx = {filename = vim.api.nvim_buf_get_name(0)}
-                ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+                local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+                ctx.dirname = vim.fn.fnamemodify(ctx.filename, ':h')
                 names = vim.tbl_filter(function(name)
                     local linter = lint.linters[name]
                     if not linter then
-                        LazyVim.warn("Linter not found: " .. name,
-                                     {title = "nvim-lint"})
+                        LazyVim.warn('Linter not found: ' .. name, { title = 'nvim-lint' })
                     end
-                    return linter and
-                               not (type(linter) == "table" and linter.condition and
-                                   not linter.condition(ctx))
+                    return linter and not (type(linter) == 'table' and linter.condition and not linter.condition(ctx))
                 end, names)
 
                 -- Run linters.
-                if #names > 0 then lint.try_lint(names) end
+                if #names > 0 then
+                    lint.try_lint(names)
+                end
             end
 
             vim.api.nvim_create_autocmd(opts.events, {
-                group = vim.api.nvim_create_augroup("nvim-lint", {clear = true}),
-                callback = M.debounce(100, M.lint)
+                group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
+                callback = M.debounce(100, M.lint),
             })
-        end
-    }
+        end,
+    },
 }
--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -8,24 +8,28 @@ return {
         cmd = {
             'MarkdownPreviewToggle',
             'MarkdownPreview',
-            'MarkdownPreviewStop'
+            'MarkdownPreviewStop',
         },
-        build = function() vim.fn['mkdp#util#install']() end,
+        build = function()
+            vim.fn['mkdp#util#install']()
+        end,
         keys = {
             {
                 '<leader>cp',
                 ft = 'markdown',
                 '<cmd>MarkdownPreviewToggle<cr>',
-                desc = 'Markdown Preview'
-            }
+                desc = 'Markdown Preview',
+            },
         },
-        config = function() vim.cmd [[do FileType]] end
+        config = function()
+            vim.cmd [[do FileType]]
+        end,
     },
     {
         'renerocksai/telekasten.nvim',
         dependencies = {
             'nvim-telescope/telescope.nvim',
-            'renerocksai/calendar-vim'
+            'renerocksai/calendar-vim',
         },
         config = function()
             require('telekasten').setup {
@@ -33,7 +37,7 @@ return {
                 dailies = home .. '/Journal/' .. 'daily',
                 weeklies = home .. '/Journal/' .. 'weekly',
                 template_new_daily = templates .. 'daily-template.md',
-                template_new_weekly = templates .. 'weekly-template.md'
+                template_new_weekly = templates .. 'weekly-template.md',
             }
 
             -- Launch panel if nothing is typed after <leader>z
@@ -41,21 +45,17 @@ return {
 
             -- Most used functions
             vim.keymap.set('n', '<leader>zf', '<cmd>Telekasten find_notes<CR>')
-            vim.keymap
-                .set('n', '<leader>zg', '<cmd>Telekasten search_notes<CR>')
+            vim.keymap.set('n', '<leader>zg', '<cmd>Telekasten search_notes<CR>')
             vim.keymap.set('n', '<leader>zd', '<cmd>Telekasten goto_today<CR>')
             vim.keymap.set('n', '<leader>zz', '<cmd>Telekasten follow_link<CR>')
             vim.keymap.set('n', '<leader>zn', '<cmd>Telekasten new_note<CR>')
-            vim.keymap.set('n', '<leader>zc',
-                           '<cmd>Telekasten show_calendar<CR>')
-            vim.keymap.set('n', '<leader>zb',
-                           '<cmd>Telekasten show_backlinks<CR>')
-            vim.keymap.set('n', '<leader>zI',
-                           '<cmd>Telekasten insert_img_link<CR>')
+            vim.keymap.set('n', '<leader>zc', '<cmd>Telekasten show_calendar<CR>')
+            vim.keymap.set('n', '<leader>zb', '<cmd>Telekasten show_backlinks<CR>')
+            vim.keymap.set('n', '<leader>zI', '<cmd>Telekasten insert_img_link<CR>')
 
             -- Call insert link automatically when we start typing a link
             vim.keymap.set('i', '[[', '<cmd>Telekasten insert_link<CR>')
-        end
+        end,
     },
     {
         'ribelo/taskwarrior.nvim',
@@ -65,6 +65,11 @@ return {
             -- refer to the configuration section below
         },
         -- or
-        config = true
-    }
+        config = true,
+    },
+    {
+        'lukas-reineke/headlines.nvim',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        config = true, -- or `opts = {}`
+    },
 }
