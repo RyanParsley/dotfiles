7379057a75302a2041c61cc778a2b0bb4e0cc653
Ryan Parsley <RMParsley@gmail.com>
2024-07-01 11:02:30 -0400

    chore: tidy up noice config

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -9,17 +9,17 @@ return {
         'MunifTanjim/nui.nvim', -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        'rcarriga/nvim-notify'
+        'rcarriga/nvim-notify',
     },
     config = function()
-        require("noice").setup({
+        require('noice').setup {
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                 override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true -- requires hrsh7th/nvim-cmp
-                }
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+                },
             },
             -- you can enable a preset for easier configuration
             presets = {
@@ -27,8 +27,8 @@ return {
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false -- add a border to hover docs and signature help
-            }
-        })
-    end
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+        }
+    end,
 }
