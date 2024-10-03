c12e26a09e4fbab365061bc73ad10ddbb03a8940
Ryan Parsley <RMParsley@gmail.com>
2024-10-03 15:53:31 -0400

    feat: swicht ot render-markdown from headlines

--- .config/nvim/lua/plugins/pkm.lua
+++ .config/nvim/lua/plugins/pkm.lua
@@ -38,6 +38,9 @@ return {
                 vim.fn.jobstart { 'open', url } -- Mac OS
                 -- vim.fn.jobstart({"xdg-open", url})  -- linux
             end,
+            ui = {
+                enable = false,
+            },
         },
         keys = {
             {
@@ -98,31 +101,10 @@ return {
         config = true,
     },
     {
-        'lukas-reineke/headlines.nvim',
-        dependencies = 'nvim-treesitter/nvim-treesitter',
-        opts = function()
-            local opts = {}
-            for _, ft in ipairs { 'markdown', 'norg', 'rmd', 'org', 'telekasten' } do
-                opts[ft] = {
-                    headline_highlights = {},
-                    -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
-                    bullets = {},
-                }
-                for i = 1, 6 do
-                    local hl = 'Headline' .. i
-                    vim.api.nvim_set_hl(0, hl, { link = 'Headline', default = true })
-                    table.insert(opts[ft].headline_highlights, hl)
-                end
-            end
-            return opts
-        end,
-        ft = { 'markdown', 'norg', 'rmd', 'org', 'telekasten' },
-        config = function(_, opts)
-            -- PERF: schedule to prevent headlines slowing down opening a file
-            vim.schedule(function()
-                require('headlines').setup(opts)
-                require('headlines').refresh()
-            end)
-        end,
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {},
+        --dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     },
 }
