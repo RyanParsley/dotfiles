16d2d4ebb5cd7f21b3a22907aa128a8e44f39f5c
Ryan Parsley <RMParsley@gmail.com>
2024-10-25 09:21:06 -0400

    feat: add copilot support

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,46 +1,125 @@
 return {
     {
-        'olimorris/codecompanion.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-treesitter/nvim-treesitter',
-            'hrsh7th/nvim-cmp', -- Optional: For activating slash commands and variables in the chat buffer
-            'nvim-telescope/telescope.nvim', -- Optional: For working with files with slash commands
-            {
-                'stevearc/dressing.nvim', -- Optional: Improves the default Neovim UI
-                opts = {},
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        build = ':Copilot auth',
+        event = 'InsertEnter',
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+            filetypes = {
+                markdown = true,
+                help = true,
             },
         },
-        config = true,
-        keys = {
-            {
-                '<leader>a',
-                '<cmd>CodeCompanionToggle<cr>',
-                desc = 'CodeCompanion Toggle',
-            },
-            {
-                '<leader>ca',
-                '<cmd>CodeCompanionActions<cr>',
-                desc = 'CodeCompanion Actions',
-            },
-            {
-                '<leader>ct',
-                '<cmd>CodeCompanionToggle<cr>',
-                desc = 'CodeCompanion Toggle',
-            },
+    },
+    {
+        'zbirenbaum/copilot-cmp',
+        dependencies = 'copilot.lua',
+        config = function(_, opts)
+            require('copilot_cmp').setup(opts)
+        end,
+    },
+    { 'AndreM222/copilot-lualine' },
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        branch = 'canary',
+        dependencies = {
+            { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+            { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
         },
         opts = {
-            strategies = {
-                chat = {
-                    adapter = 'gemini',
-                },
-                inline = {
-                    adapter = 'gemini',
-                },
-                agent = {
-                    adapter = 'gemini',
-                },
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+        keys = {
+            -- Show help actions with telescope
+            {
+                '<leader>ah',
+                function()
+                    local actions = require 'CopilotChat.actions'
+                    require('CopilotChat.integrations.telescope').pick(actions.help_actions())
+                end,
+                desc = 'CopilotChat - Help actions',
             },
+            -- Show prompts actions with telescope
+            {
+                '<leader>ap',
+                function()
+                    local actions = require 'CopilotChat.actions'
+                    require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
+                end,
+                desc = 'CopilotChat - Prompt actions',
+            },
+            {
+                '<leader>ap',
+                ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
+                mode = 'x',
+                desc = 'CopilotChat - Prompt actions',
+            },
+            -- Code related commands
+            { '<leader>ae', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
+            { '<leader>at', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
+            { '<leader>ar', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code' },
+            { '<leader>aR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code' },
+            { '<leader>an', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Naming' },
+            -- Chat with Copilot in visual mode
+            {
+                '<leader>av',
+                ':CopilotChatVisual',
+                mode = 'x',
+                desc = 'CopilotChat - Open in vertical split',
+            },
+            {
+                '<leader>ax',
+                ':CopilotChatInline<cr>',
+                mode = 'x',
+                desc = 'CopilotChat - Inline chat',
+            },
+            -- Custom input for CopilotChat
+            {
+                '<leader>ai',
+                function()
+                    local input = vim.fn.input 'Ask Copilot: '
+                    if input ~= '' then
+                        vim.cmd('CopilotChat ' .. input)
+                    end
+                end,
+                desc = 'CopilotChat - Ask input',
+            },
+            -- Generate commit message based on the git diff
+            {
+                '<leader>am',
+                '<cmd>CopilotChatCommit<cr>',
+                desc = 'CopilotChat - Generate commit message for all changes',
+            },
+            {
+                '<leader>aM',
+                '<cmd>CopilotChatCommitStaged<cr>',
+                desc = 'CopilotChat - Generate commit message for staged changes',
+            },
+            -- Quick chat with Copilot
+            {
+                '<leader>aq',
+                function()
+                    local input = vim.fn.input 'Quick Chat: '
+                    if input ~= '' then
+                        vim.cmd('CopilotChatBuffer ' .. input)
+                    end
+                end,
+                desc = 'CopilotChat - Quick chat',
+            },
+            -- Debug
+            { '<leader>ad', '<cmd>CopilotChatDebugInfo<cr>', desc = 'CopilotChat - Debug Info' },
+            -- Fix the issue with diagnostic
+            { '<leader>af', '<cmd>CopilotChatFixDiagnostic<cr>', desc = 'CopilotChat - Fix Diagnostic' },
+            -- Clear buffer and chat history
+            { '<leader>al', '<cmd>CopilotChatReset<cr>', desc = 'CopilotChat - Clear buffer and chat history' },
+            -- Toggle Copilot Chat Vsplit
+            { '<leader>av', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle' },
+            -- Copilot Chat Models
+            { '<leader>a?', '<cmd>CopilotChatModels<cr>', desc = 'CopilotChat - Select Models' },
         },
     },
     {
@@ -70,7 +149,15 @@ return {
             --    you can use this plugin to help you. It even has snippets
             --    for various frameworks/libraries/etc. but you will have to
             --    set up the ones that are useful for you.
-            -- 'rafamadriz/friendly-snippets',
+            'rafamadriz/friendly-snippets',
+            {
+                'zbirenbaum/copilot-cmp',
+                dependencies = 'copilot.lua',
+                config = function(_, opts)
+                    local copilot_cmp = require 'copilot_cmp'
+                    copilot_cmp.setup(opts)
+                end,
+            },
         },
         config = function()
             -- See `:help cmp`
@@ -135,6 +222,7 @@ return {
                 sources = {
                     { name = 'path' },
                     { name = 'buffer' },
+                    { name = 'copilot' },
                     { name = 'luasnip' },
                     {
                         name = 'nvim_lsp',
