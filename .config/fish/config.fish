027cf579887bedf9f6c85381b0cbd94f84d10ee2
Ryan Parsley <RMParsley@gmail.com>
2024-10-29 09:32:24 -0400

    feat: add starship init to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "/Users/ryan/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
+starship init fish | source
