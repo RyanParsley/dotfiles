fca2d1d667bf96a56c19f18540b637a368303275
Ryan Parsley <RMParsley@gmail.com>
2024-11-06 15:43:00 -0500

    feat: move copilot config to local config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -17,7 +17,6 @@ return {
         'MunifTanjim/nui.nvim',
         --- The below dependencies are optional,
         'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-        'zbirenbaum/copilot.lua', -- for providers='copilot'
         {
             -- support for image pasting
             'HakonHarnes/img-clip.nvim',
