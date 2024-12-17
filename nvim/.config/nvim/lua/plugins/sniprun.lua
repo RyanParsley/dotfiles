4bbfeed0d0982918a969a3ee9dd53b81f40b85e4
Ryan Parsley <RMParsley@gmail.com>
2024-12-17 13:29:08 -0500

    chore: file got formatted on touch

--- nvim/.config/nvim/lua/plugins/sniprun.lua
+++ nvim/.config/nvim/lua/plugins/sniprun.lua
@@ -8,37 +8,41 @@ return {
 
     config = function()
         require('sniprun').setup {
-            display = {"Terminal"},
+            display = { 'Terminal' },
             display_options = {
                 terminal_scrollback = vim.o.scrollback, -- change terminal display scrollback lines
                 terminal_line_number = false, -- whether show line number in terminal window
                 terminal_signcolumn = false, -- whether show signcolumn in terminal window
-                terminal_position = "vertical", -- # or "horizontal", to open as horizontal split instead of vertical split
+                terminal_position = 'vertical', -- # or "horizontal", to open as horizontal split instead of vertical split
                 terminal_width = 45, -- # change the terminal display option width (if vertical)
-                terminal_height = 20 -- # change the terminal display option height (if horizontal)
+                terminal_height = 20, -- # change the terminal display option height (if horizontal)
             },
             interpreter_options = {
                 Generic = {
                     nushell = {
-                        supported_filetypes = {"nu"},
-                        extension = ".nu",
+                        supported_filetypes = { 'nu' },
+                        extension = '.nu',
 
-                        interpreter = "nu",
-                        compiler = "",
+                        interpreter = 'nu',
+                        compiler = '',
 
-                        exe_name = "",
-                        boilerplate_pre = "",
-                        boilerplate_post = ""
-                    }
+                        exe_name = '',
+                        boilerplate_pre = '',
+                        boilerplate_post = '',
+                    },
                 },
                 GFM_original = {
                     use_on_filetypes = {
-                        'markdown.pandoc', 'markdown', 'vimwiki', 'telekasten',
-                        'markdown_fenced_languages', 'markdown_inline'
+                        'markdown.pandoc',
+                        'markdown',
+                        'vimwiki',
+                        'telekasten',
+                        'markdown_fenced_languages',
+                        'markdown_inline',
                     },
-                    default_filetype = 'typescript' -- default filetype (not github flavored markdown name)
-                }
-            }
+                    default_filetype = 'typescript', -- default filetype (not github flavored markdown name)
+                },
+            },
         }
-    end
+    end,
 }
