1502333c4cefb2db8e777fe58316c77802b895ce
Ryan Parsley <RMParsley@gmail.com>
2025-02-13 09:40:37 -0500

    feat: take control of completion order by source

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -96,11 +96,8 @@ return {
                         end
                     end, { 'i', 's' }),
                 },
-                sources = {
-                    { name = 'path' },
-                    { name = 'buffer' },
-                    { name = 'crates' },
-                    { name = 'luasnip' },
+                sources = cmp.config.sources {
+                    { name = 'nvim_lua' },
                     {
                         name = 'nvim_lsp',
                         option = {
@@ -109,7 +106,38 @@ return {
                             },
                         },
                     },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'buffer' },
+                    { name = 'crates' },
                     vim.g.copilotEnabled and { name = 'copilot' } or {},
+                },
+                sorting = {
+                    -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+                    comparators = {
+                        cmp.config.compare.offset,
+                        cmp.config.compare.exact,
+                        cmp.config.compare.score,
+
+                        -- copied from cmp-under, but I don't think I need the plugin for this.
+                        -- I might add some more of my own.
+                        function(entry1, entry2)
+                            local _, entry1_under = entry1.completion_item.label:find '^_+'
+                            local _, entry2_under = entry2.completion_item.label:find '^_+'
+                            entry1_under = entry1_under or 0
+                            entry2_under = entry2_under or 0
+                            if entry1_under > entry2_under then
+                                return false
+                            elseif entry1_under < entry2_under then
+                                return true
+                            end
+                        end,
+
+                        cmp.config.compare.kind,
+                        cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
+                    },
                 },
             }
         end,
