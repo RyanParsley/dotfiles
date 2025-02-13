ade6f9cb52957dcae0a92fe5a381993d82c073af
Ryan Parsley <RMParsley@gmail.com>
2025-02-13 09:39:01 -0500

    feat: render markdown in codecompanion chats

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,13 +1,12 @@
 return {
-    {
-        'olimorris/codecompanion.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-treesitter/nvim-treesitter',
-            'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
-            'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
-            { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
-        },
-        config = true,
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+        'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
+        'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
+        { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
+        { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
     },
+    config = true,
 }
