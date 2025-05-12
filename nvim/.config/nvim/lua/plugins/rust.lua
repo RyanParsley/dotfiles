fcfc6cf6d6729f84726dc34a661926a2feb0b9c8
Ryan Parsley <RMParsley@gmail.com>
2025-05-12 09:06:16 -0400

    chore: version bump rustacean dependency

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,7 +1,7 @@
 return {
     {
         'mrcjkb/rustaceanvim',
-        version = '^5', -- Recommended
+        version = '^6', -- Recommended
         ft = { 'rust' },
         lazy = false, -- This plugin is already lazy
         ['rust-analyzer'] = {
