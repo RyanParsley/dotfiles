824e20cdde40d8d3040313678c611802bc14e2ad
Ryan Parsley <RMParsley@gmail.com>
2025-05-20 09:46:58 -0400

    chore: sync with default config post update

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -27,29 +27,29 @@ return {
                     },
                 },
             }
-            local mason_registry = require 'mason-registry'
-            local codelldb = mason_registry.get_package 'codelldb'
-            local extension_path = codelldb:get_install_path() .. '/extensions/'
+            vim.g.rustaceanvim = function()
+                -- Update this path
+                local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/'
+                local codelldb_path = extension_path .. 'adapter/codelldb'
+                local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+                local this_os = vim.uv.os_uname().sysname
 
-            local codelldb_path = extension_path .. 'adapter/codelldb'
-            local liblldb_path = extension_path .. 'lldb/lib/liblldb'
-            local this_os = vim.uv.os_uname().sysname
+                -- The path is different on Windows
+                if this_os:find 'Windows' then
+                    codelldb_path = extension_path .. 'adapter\\codelldb.exe'
+                    liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
+                else
+                    -- The liblldb extension is .so for Linux and .dylib for MacOS
+                    liblldb_path = liblldb_path .. (this_os == 'Linux' and '.so' or '.dylib')
+                end
 
-            -- The path is different on Windows
-            if this_os:find 'Windows' then
-                codelldb_path = extension_path .. 'adapter\\codelldb.exe'
-                liblldb_path = extension_path .. 'lldb\\bin\\liblldb.dll'
-            else
-                -- The liblldb extension is .so for Linux and .dylib for MacOS
-                liblldb_path = liblldb_path .. (this_os == 'Linux' and '.so' or '.dylib')
+                local cfg = require 'rustaceanvim.config'
+                return {
+                    dap = {
+                        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+                    },
+                }
             end
-
-            local cfg = require 'rustaceanvim.config'
-            return {
-                dap = {
-                    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-                },
-            }
         end,
     },
     {
