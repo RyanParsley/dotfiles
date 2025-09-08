84d84452a33fc5aa82722bd3558c91812a364894
Ryan Parsley <RMParsley@gmail.com>
2025-09-08 06:24:57 -0400

    feat: add lmstudio to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,8 @@ starship init fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/ryan/.lmstudio/bin
+# End of LM Studio CLI section
+
